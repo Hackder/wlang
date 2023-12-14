f91284c737f9ec8ba838e6d318957e0e57cf7ba3
Juraj Petráš <jurajpetras@Jurajs-MacBook-Pro-2.local>
2023-12-14 03:23:01 +0100

    yeet WLang++

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#define wio
+#include "wlang.h"
+
+
+facts isprime oh basic n shit spread
+  bet oh n smool 2 shit yeet cap rn
+
+  slay oh basic i be 2 rn i smool n rn i grow shit spread
+    bet oh n mood i vibecheck 0 shit yeet cap rn
+  unspread
+
+  yeet nocap rn
+unspread
+
+basic g bot 5 nobot bot 5 nobot be spread
+  spread 0, 1, 1, 0, 0 unspread,
+  spread 1, 0, 0, 0, 0 unspread,
+  spread 1, 0, 0, 0, 0 unspread,
+  spread 0, 1, 1, 0, 0 unspread,
+  spread 0, 0, 0, 0, 0 unspread,
+unspread rn
+
+basic dfs oh basic source, facts ther visited shit spread
+  basic total be 1 rn
+
+  visited bot source nobot be nocap rn
+
+  slay oh basic i be 0 rn i smool 5 rn i grow shit spread
+    bet oh nah g bot source nobot bot i nobot shit slide rn
+
+    bet oh nah visited bot i nobot shit spread
+      total be total slap dfs oh i, visited shit rn
+    unspread
+  unspread
+
+  yeet total rn
+unspread
+
+flex node spread
+  node ther next rn
+  basic val rn
+unspread rn
+
+flex queue spread
+  node ther front rn 
+  node ther back rn
+
+  void insert oh basic value shit rn
+  basic pop oh shit rn
+unspread rn
+
+void queue rizz insert oh basic value shit spread
+  node ther n be birth node oh shit rn
+
+  bet oh me->back vibecheck hobo shit spread
+    me->front be n rn
+    me->back be n rn
+    yeet rn
+  unspread
+
+  n->next be hobo rn
+  n->val be value rn
+
+  me->back->next be n rn
+  me->back be n rn
+unspread
+
+basic queue rizz pop oh shit spread
+  bet oh me->back vibecheck hobo shit yeet 0 rn
+
+  basic value be me->front->val;
+
+  bet oh me->back vibecheck me->front shit spread
+    kys me->back rn
+    me->back be hobo rn
+    me->front be hobo rn
+
+    yeet value rn
+  unspread
+
+  node ther temp be me->front rn
+  me->front be me->front->next rn 
+  kys temp rn
+
+  yeet value rn
+unspread
+
+basic main oh shit spread
+  facts visited bot 5 nobot rn
+  slay oh basic i be 0 rn i smool 5 rn i grow shit visited bot i nobot be cap rn
+
+  basic result be dfs oh 0, oh facts ther shit wher visited shit rn
+
+  cum result bruh rn
+
+  yeet 0 rn
+unspread
---
+++ wlang.h
@@ -0,0 +1,57 @@
+#define basic int
+#define facts bool
+#define be =
+#define rn ;
+#define oh (
+#define shit )
+#define spread {
+#define unspread }
+#define bot [
+#define nobot ]
+#define yeet return
+#define steal include
+#define nocap true
+#define cap false
+#define valid &&
+#define nah !
+#define vibecheck ==
+
+#define flex struct
+#define rizz ::
+
+#define slap +
+#define sub -
+#define cutup /
+#define ther *
+#define times *
+#define mood %
+#define grow ++
+#define shrink --
+#define wher &
+
+#define smol <=
+#define bik >=
+#define smool <
+#define biik >
+
+#define in >>
+#define out <<
+
+#ifdef wio
+#define suck std::cin
+#define cum std::cout <<
+#define bruh << std::endl
+#endif
+
+#define slay for
+#define bet if
+#define yikes else
+#define gtfo break
+#define slide continue
+
+#define bussin while (true)
+#define yolo auto
+#define hobo nullptr
+#define me this
+#define birth new
+#define kys delete
